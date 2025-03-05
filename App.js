560791600bdccb5ba33a70dc8d5ecb9dccf5fa59
manoelkenpachi <manoelkenpachi@gmail.com>
2025-03-05 00:52:14 -0300

    feat: configura navegação e provider de autenticação

--- App.js
+++ App.js
@@ -2,8 +2,8 @@ import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 import WorkoutScreen from "./src/WorkoutScreen/index.js";
-
 import LoginScreen from "./src/LoginScreen/index.js";
+import CreateWorkoutScreen from "./src/CreateWorkoutScreen/index.js";
 import { AuthProvider } from "./src/AuthContext/index.js";
 
 const Stack = createStackNavigator();
@@ -12,9 +12,31 @@ export default function App() {
   return (
     <AuthProvider>
       <NavigationContainer>
-        <Stack.Navigator initialRouteName="LoginScreen">
-          <Stack.Screen name="LoginScreen" component={LoginScreen} options={{ title: "Login" }} />
-          <Stack.Screen name="WorkoutScreen" component={WorkoutScreen} options={{ title: "Meu Treino" }} />
+        <Stack.Navigator 
+          initialRouteName="LoginScreen"
+          screenOptions={{
+            headerStyle: {
+              backgroundColor: '#121212',
+            },
+            headerTintColor: '#BB86FC',
+            headerTitleStyle: {
+              fontWeight: 'bold',
+            },
+            headerShown: false
+          }}
+        >
+          <Stack.Screen 
+            name="LoginScreen" 
+            component={LoginScreen} 
+          />
+          <Stack.Screen 
+            name="WorkoutScreen" 
+            component={WorkoutScreen} 
+          />
+          <Stack.Screen 
+            name="CreateWorkout" 
+            component={CreateWorkoutScreen} 
+          />
         </Stack.Navigator>
       </NavigationContainer>
     </AuthProvider>
